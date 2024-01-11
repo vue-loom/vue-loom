88006b6306fc28ecb97610eb24e225865e7d107e
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2024-01-11 16:16:29 +0200

    Update colors.ts

--- src/partials/colors.ts
+++ src/partials/colors.ts
@@ -1,4 +1,5 @@
 import type {Theme} from '../component-types/theme.ts';
+
 const resolveBg = (color: string | 'primary' | 'secondary' | 'accent' | 'success' | 'warning' | 'danger'): string => {
     const theme: Theme = {
         primary: 'bg-primary',
