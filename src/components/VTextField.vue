da0153d4d5623b1556eefb3078cc270250b24181
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2024-03-08 12:09:51 +0200

    Removed TextField css

--- src/components/VTextField.vue
+++ src/components/VTextField.vue
@@ -132,15 +132,5 @@
 </template>
 
 <style scoped>
-    /* Chrome, Safari, Edge, Opera */
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
 
-    /* Firefox */
-    input[type=number] {
-        -moz-appearance: textfield;
-    }
 </style>
