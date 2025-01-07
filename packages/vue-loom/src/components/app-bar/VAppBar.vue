5500ef6f30f79cc17b2fec3d1ddd5841b14863cb
Danie Nortje <>
2025-01-07 10:39:00 +0200

    VAppBar WIP

--- packages/vue-loom/src/components/app-bar/VAppBar.vue
+++ packages/vue-loom/src/components/app-bar/VAppBar.vue
@@ -21,27 +21,31 @@
 </script>
 
 <template>
-    <VCard :class="[cn('rounded-none border-b border-x-0 p-0 sticky top-0 z-30 min-h-16 flex justify-center', props.class)]">
+    <VCard :class="[cn('rounded-none border-x-0 border-t-0 p-0 sticky top-0 z-30 min-h-16 flex justify-center', props.class)]">
         <VCardContent>
             <div class="flex items-center md:px-6 lg:px-8">
-                <div class="grow flex space-x-4 items-center">
-                    <div v-if="logoSrc" class="max-h-16 py-2">
-                        <img :src="logoSrc" alt="App bar logo image">
-                    </div>
+                <div class="grow flex space-x-4 items-center justify-center md:justify-start">
+                    <slot name="logo">
+                        <div v-if="logoSrc" class="max-h-16 py-2 hidden md:block">
+                            <img :src="logoSrc" alt="App bar logo image">
+                        </div>
+                    </slot>
                     <h1 v-if="title" class="text-2xl py-2">
                         {{ title }}
                     </h1>
-                    <div class="flex h-full">
-                        <div v-for="item in items?.slice(0,3)"
-                             class="h-full p-2 cursor-pointer hover:underline transition-all duration-75"
-                             :class="[item.active ? 'underline text-primary' : '']"
-                             @click="goToPage(item)"
-                        >
-                            {{ item.label }}
+                    <div class="hidden md:block">
+                        <div class="flex">
+                            <div v-for="item in items?.slice(0,3)"
+                                 class="h-full p-2 cursor-pointer hover:underline transition-all duration-75"
+                                 :class="[item.active ? 'underline text-primary' : '']"
+                                 @click="goToPage(item)"
+                            >
+                                {{ item.label }}
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="shrink">
+                <div class="shrink hidden md:block">
                     <slot name="actions"/>
                 </div>
             </div>
