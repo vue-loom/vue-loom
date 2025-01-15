044d00899536c0bc7f1c01722b24f11004eae2af
Edward van Tonder <56725833+EA-Wardie@users.noreply.github.com>
2025-01-15 14:42:19 +0200

    Multi select fixes

--- src/components/Sandbox.vue
+++ src/components/Sandbox.vue
@@ -147,6 +147,12 @@
     const selectItems = [
         {id: 1, name: 'Item 1'},
         {id: 2, name: 'Item 2'},
+        {id: 3, name: 'Item 3'},
+        {id: 4, name: 'Item 4'},
+        {id: 5, name: 'Item 5'},
+        {id: 6, name: 'Item 6'},
+        {id: 7, name: 'Item 7'},
+        {id: 8, name: 'Item 8'},
     ];
 </script>
 
@@ -179,7 +185,7 @@
                 <v-button>Toggle App Menu</v-button>
             </v-app-menu-trigger>
 
-            <v-card>
+            <v-card class="grid grid-cols-4">
                 <v-select placeholder="Select" :items="selectItems"/>
                 <v-multi-select placeholder="Select" :items="selectItems"/>
                 <v-input placeholder="Input"/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const animate = require("tailwindcss-animate")
 const typography = require("@tailwindcss/typography")
+const forms = require("@tailwindcss/forms")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -92,5 +93,5 @@ module.exports = {
             },
         },
     },
-    plugins: [animate, typography],
+    plugins: [animate, typography, forms],
 }
