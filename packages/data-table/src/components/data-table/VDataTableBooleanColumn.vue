704f4f3eaeef5d1a35c0828aa715e9e45a5368f1
Stian Scholtz <>
2025-03-03 10:12:48 +0200

    Fixed bug where boolean column show yes for falsey values. Add extensive support to handle any truthy/falsy value correctly.

--- packages/data-table/src/components/data-table/VDataTableBooleanColumn.vue
+++ packages/data-table/src/components/data-table/VDataTableBooleanColumn.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
-    withDefaults(defineProps<{ value: 1 | 0 | boolean | 'yes' | 'no' | 'Yes' | 'No' }>(), {
-        value: false,
+    import {computed} from "vue";
+
+    const props = withDefaults(defineProps<{ value: 1 | 0 | '1' | '0' | boolean | 'yes' | 'no' | 'Yes' | 'No' | 'y' | 'n' | 'Y' | 'N' }>(), {
+      value: false,
+    });
+
+    const isTruthy = computed(() => {
+      if (typeof props.value === 'string') {
+        return ['1', 'y', 'yes'].includes(props.value.toLowerCase());
+      }
+      return Boolean(props.value);
     });
 </script>
 
 <template>
     <div class="w-fit border rounded text-xs uppercase leading-none px-1 pt-[3px] pb-0.5"
-         :class="[!!value || (value || '').toString().toLowerCase() === 'yes' ? 'bg-emerald-500/5 border-emerald-600 text-emerald-600' : 'bg-destructive/5 border-destructive text-destructive']">
-        {{ !!value || (value || '').toString().toLowerCase() === 'yes' ? 'Yes' : 'No' }}
+         :class="[isTruthy ? 'bg-emerald-500/5 border-emerald-600 text-emerald-600' : 'bg-destructive/5 border-destructive text-destructive']">
+        {{ isTruthy ? 'Yes' : 'No' }}
     </div>
 </template>
 
